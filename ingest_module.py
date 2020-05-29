0a5cddc46d93797f384e0f37c684b797558f4698
ricardoapl <ricardoapl@protonmail.com>
2020-05-29 11:46:28 +0100

    Add data source ingest module analysis

--- ingest_module.py
+++ ingest_module.py
@@ -1,10 +1,8 @@
 import os
-import jarray
 import inspect
 
 import java.util.ArrayList as ArrayList
 from java.io import File
-from java.lang import Class
 from java.lang import System
 from java.lang import ProcessBuilder
 from java.util.logging import Level
@@ -13,35 +11,33 @@ from org.sleuthkit.datamodel import AbstractFile
 from org.sleuthkit.datamodel import ReadContentInputStream
 from org.sleuthkit.datamodel import BlackboardArtifact
 from org.sleuthkit.datamodel import BlackboardAttribute
-from org.sleuthkit.datamodel import Image
 from org.sleuthkit.autopsy.ingest import IngestModule
 from org.sleuthkit.autopsy.ingest import IngestJobContext
 from org.sleuthkit.autopsy.ingest.IngestModule import IngestModuleException
 from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
 from org.sleuthkit.autopsy.ingest import DataSourceIngestModuleProcessTerminator
-from org.sleuthkit.autopsy.ingest import FileIngestModule
 from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
 from org.sleuthkit.autopsy.ingest import IngestMessage
 from org.sleuthkit.autopsy.ingest import IngestServices
 from org.sleuthkit.autopsy.coreutils import PlatformUtil
 from org.sleuthkit.autopsy.coreutils import Logger
+from org.sleuthkit.autopsy.coreutils import ExecUtil
+from org.sleuthkit.autopsy.datamodel import ContentUtils
 from org.sleuthkit.autopsy.casemodule import Case
 from org.sleuthkit.autopsy.casemodule.services import Services
 from org.sleuthkit.autopsy.casemodule.services import FileManager
 from org.sleuthkit.autopsy.casemodule.services import Blackboard
 
 
-# Factory that defines the name and details of the module and allows Autopsy
-# to create instances of the modules that will do the analysis.
-class FBMessengerIngestModuleFactory(IngestModuleFactoryAdapter):
+class W10FaceMessengerIngestModuleFactory(IngestModuleFactoryAdapter):
 
-    moduleName = "FBMessenger Data Source Module"
+    moduleName = "W10-FaceMessenger @ Autopsy"
 
     def getModuleDisplayName(self):
         return self.moduleName
 
     def getModuleDescription(self):
-        return "Extract artifacts produced by Facebook Messenger (Beta)."
+        return "Wrapper module for W10-FaceMessenger."
 
     def getModuleVersionNumber(self):
         return "1.0"
@@ -50,13 +46,12 @@ class FBMessengerIngestModuleFactory(IngestModuleFactoryAdapter):
         return True
 
     def createDataSourceIngestModule(self, ingestOptions):
-        return FBMessengerIngestModule()
+        return W10FaceMessengerIngestModule()
 
 
-# Data Source-level ingest module.  One gets created per data source.
-class FBMessengerIngestModule(DataSourceIngestModule):
+class W10FaceMessengerIngestModule(DataSourceIngestModule):
 
-    _logger = Logger.getLogger(FBMessengerIngestModuleFactory.moduleName)
+    _logger = Logger.getLogger(W10FaceMessengerIngestModuleFactory.moduleName)
 
     def log(self, level, msg):
         self._logger.logp(level, self.__class__.__name__, inspect.stack()[1][3], msg)
@@ -65,62 +60,130 @@ class FBMessengerIngestModule(DataSourceIngestModule):
         self.context = None
 
     # Where any setup and configuration is done
-    # 'context' is an instance of org.sleuthkit.autopsy.ingest.IngestJobContext.
-    # See: http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_ingest_job_context.html
+    # 'context' is an instance of org.sleuthkit.autopsy.ingest.IngestJobContext
+    # See http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_ingest_job_context.html
     def startUp(self, context):
         self.context = context
-    
-        # Get path to FBMessenger based on where this script is run from.
-        # Assumes FBMessenger is in same folder as script
-        # Verify it is there before any ingest starts
-        fbmessenger_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"fbmessenger\__main__.py")
-        self.path_to_fbmessenger = File(fbmessenger_path)
-        if not self.path_to_fbmessenger.exists():
-            raise IngestModuleException("FBMessenger was not found in the module folder")
+        self.pathToEXE = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"w10-facemessenger\w10-facemessenger.exe")
+        if not os.path.exists(self.pathToEXE):
+            raise IngestModuleException("W10-FaceMessenger was not found in module folder")
 
-    # Where the analysis is done.
-    # The 'dataSource' object being passed in is of type org.sleuthkit.datamodel.Content.
-    # See: http://www.sleuthkit.org/sleuthkit/docs/jni-docs/latest/interfaceorg_1_1sleuthkit_1_1datamodel_1_1_content.html
+    # Where the analysis is done
+    # The 'dataSource' object being passed in is of type org.sleuthkit.datamodel.Content
+    # See http://www.sleuthkit.org/sleuthkit/docs/jni-docs/latest/interfaceorg_1_1sleuthkit_1_1datamodel_1_1_content.html
     # 'progressBar' is of type org.sleuthkit.autopsy.ingest.DataSourceIngestModuleProgress
-    # See: http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_data_source_ingest_module_progress.html
+    # See http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_data_source_ingest_module_progress.html
     def process(self, dataSource, progressBar):
 
-        # we don't know how much work there is yet
-        progressBar.switchToIndeterminate()
-
-        # We make use of hindsight Windows EXE, so bail if we aren't on Windows
         if not PlatformUtil.isWindowsOS(): 
             self.log(Level.INFO, "Ignoring data source. Not running on Windows")
             return IngestModule.ProcessResult.OK
 
-        # Verify we have a disk image and not a folder of files
-        if not isinstance(dataSource, Image):
-            self.log(Level.INFO, "Ignoring data source.  Not an image")
-            return IngestModule.ProcessResult.OK
-
-        imagePaths = dataSource.getPaths()
-
-        # TODO (ricardoapl) Add arguments for HTML report
-        # Run the FBMessenger, saving output to reportFile
-        # We use ExecUtil because it will deal with the user cancelling the job
-        self.log(Level.INFO, "Running FBMessenger on data source")
-        cmd = ArrayList()
-        cmd.add(self.path_to_fbmessenger.toString())
-        # Add each argument in its own line. I.e. "-f foo" would be two calls to .add()
-        cmd.add(imagePaths[0])
+        progressBar.switchToIndeterminate()
         
-        processBuilder = ProcessBuilder(cmd)
-        processBuilder.redirectOutput(reportFile)
-        ExecUtil.execute(processBuilder, DataSourceIngestModuleProcessTerminator(self.context))
+        # Find user profile parent directories (%SystemDrive%\Users)
+        fileManager = Case.getCurrentCase().getServices().getFileManager()
+        directories = fileManager.findFiles(dataSource, "Users")
 
-        # TODO (ricardoapl) Move FBMessenger resulting files to case report dir
-        # Add the report to the case, so it shows up in the tree
-        # Do not add report to the case tree if the ingest is cancelled before finish.
-        if not self.context.dataSourceIngestIsCancelled():
-            Case.getCurrentCase().addReport(reportFile.toString(), "Run EXE", "img_stat output")
-        else:
-            if reportFile.exists():
-                if not reportFile.delete():
-                    self.log(LEVEL.warning,"Error deleting the incomplete report file")
+        numDirectories = len(directories)
+        progressBar.switchToDeterminate(numDirectories)
+        directoryCount = 0
+        for directory in directories:
 
+            # Check if the user pressed cancel while we were busy
+            if self.context.isJobCancelled():
+                return IngestModule.ProcessResult.OK
+            
+            # Report work progress to end user
+            directoryCount += 1
+            progressBar.progress(directoryCount)
+            self.log(Level.INFO, "Processing directory {} of {}".format(directoryCount, numDirectories))
+            
+            # Ignore false positives
+            if not directory.isDir():
+                continue
+
+            # Where the (real) analysis happens
+            self._processContent(directory)
+            
+        # Once we are done, post a message to the ingest messages inbox
+        messageType = IngestMessage.MessageType.DATA
+        messageSource = W10FaceMessengerIngestModuleFactory.moduleName
+        messageSubject = "Finished analysis! Please see <<Reports>>"
+        message = IngestMessage.createMessage(messageType, messageSource, messageSubject)
+        IngestServices.getInstance().postMessage(message)
         return IngestModule.ProcessResult.OK
+
+    def _processContent(self, directory):
+        
+        # Create temporary directory to extract content to
+        caseTempDirectory = Case.getCurrentCase().getTempDirectory()
+        tempDirectoryName = str(directory.getId())
+        tempDirectory = os.path.join(caseTempDirectory, tempDirectoryName)
+        
+        self.log(Level.INFO, "Creating temporary directory " + tempDirectory)
+        os.mkdir(tempDirectory)
+
+        self._extractContent(directory, tempDirectory)
+        self._analyzeContent(tempDirectory)
+
+    # Extract data from Autopsy into filesystem for later analysis
+    # The 'content' object being passed in is of type org.sleuthkit.datamodel.Content
+    # See http://sleuthkit.org/sleuthkit/docs/jni-docs/4.9.0//classorg_1_1sleuthkit_1_1datamodel_1_1_abstract_content.html
+    def _extractContent(self, content, path):
+        children = content.getChildren()
+        for child in children:
+            childName = child.getName()
+            if childName == "." or childName == "..":
+                continue
+            elif child.isFile():
+                childPath = os.path.join(path, childName)
+                ContentUtils.writeToFile(child, File(childPath))
+            elif child.isDir():
+                childPath = os.path.join(path, childName)
+                os.mkdir(childPath)
+                self._extractContent(child, childPath)
+
+    # XXX (ricardoapl) We assume that path has been previously populated with user profile directories
+    def _analyzeContent(self, path):
+
+        # Ignore false positives
+        profiles = [directory for directory in os.listdir(path) if os.path.isdir(os.path.join(path, directory))]
+        
+        for profile in profiles:
+
+            caseReportDirectory = Case.getCurrentCase().getReportDirectory()
+            reportDirectoryName = os.path.basename(path) + "-" + profile
+            reportDirectory = os.path.join(caseReportDirectory, reportDirectoryName)
+            os.mkdir(reportDirectory)
+            profileDirectory = os.path.join(path, profile)
+
+            # Run the W10-FaceMessenger EXE, saving output to reportDirectory
+            # We use ExecUtil because it will deal with the user cancelling the job
+            self.log(Level.INFO, "Running W10-FaceMessenger on profile " + reportDirectoryName)
+            cmd = ArrayList()
+            cmd.add(self.pathToEXE)
+            cmd.add("--input")
+            cmd.add(profileDirectory)
+            cmd.add("--output")
+            cmd.add(reportDirectory)
+            
+            # TODO (ricardoapl) Add depth argument according to module settings
+
+            processBuilder = ProcessBuilder(cmd)
+            ExecUtil.execute(processBuilder, DataSourceIngestModuleProcessTerminator(self.context))
+            
+            # TODO (ricardoapl) Remove directories for which no report was produced
+
+            # Add the report to the case, so it shows up in the tree
+            # Do not add report to the case tree if the ingest is cancelled before finish
+            if not self.context.dataSourceIngestIsCancelled():
+                reportFileName = os.path.join(reportDirectory, "report\\report.html")
+                reportModuleName = W10FaceMessengerIngestModuleFactory.moduleName
+                reportName = "W10-FaceMessenger " + reportDirectoryName
+                Case.getCurrentCase().addReport(reportFileName, reportModuleName, reportName)
+            else:
+                reportFile = File(reportFileName)
+                if reportFile.exists():
+                    if not reportFile.delete():
+                        self.log(LEVEL.warning, "Error deleting the incomplete report file")
